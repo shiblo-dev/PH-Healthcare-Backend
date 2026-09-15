13a90cf0735f5c4552eda95095299b1e1d3bbfaa
shiblo-dev <devshiblo6@gmail.com>
2026-09-15 12:17:15 +0600

    log out route created

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -41,4 +41,8 @@ router.post(
 	validateRequest(UserValidation.ResetPasswordZodSchema),
 	AuthController.resetPassword,
 );
+router.post(
+	"/logout",
+ 	AuthController.logout,
+);
 export const AuthRoutes = router;
